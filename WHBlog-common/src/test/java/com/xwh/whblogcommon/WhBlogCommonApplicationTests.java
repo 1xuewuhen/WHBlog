0f519ec91acf66a658d08323bdf00fd9b1f53335
血无痕 <cfy555388com@163.com>
2023-04-02 08:41:29 +0800

    项目初始化

---
+++ WHBlog-common/src/test/java/com/xwh/whblogcommon/WhBlogCommonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xwh.whblogcommon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WhBlogCommonApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
